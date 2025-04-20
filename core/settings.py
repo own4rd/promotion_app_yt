7f9ad64d3ac24ea9ae4b0dfd9640756c0a052f2e
Wagner <wagnerpalmeira.ti@gmail.com>
2025-04-20 18:12:13 -0300

    Merge done

--- core/settings.py
+++ core/settings.py
@@ -134,11 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = [
     "127.0.0.1", #.env
-<<<<<<< Updated upstream
 ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-=======
-]
->>>>>>> Stashed changes
