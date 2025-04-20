97f2fe32eafe3a72efda77ec9bfca88859f83f1b
Wagner <wagnerpalmeira.ti@gmail.com>
2025-04-20 18:12:44 -0300

    Merge done

--- core/urls.py
+++ core/urls.py
@@ -2,19 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 from debug_toolbar.toolbar import debug_toolbar_urls
-<<<<<<< Updated upstream
 from django.conf import settings
 from django.conf.urls.static import static
-=======
->>>>>>> Stashed changes
 
 urlpatterns = [
     path('', views.home_view),
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls()
-<<<<<<< Updated upstream
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
->>>>>>> Stashed changes
